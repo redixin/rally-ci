4b1b374ba8472619ab4299d9eba475a60a215d03
Sergey Skripnick <sskripnick@mirantis.com>
2014-11-10 17:39:37 +0200

    Imporve fancynginx logger
    Add publish_summary method
    Add render method

--- fancynginx.py
+++ fancynginx.py
@@ -19,31 +19,41 @@ def mkdir(path):
 
 
 class Driver(object):
+    """Job results publisher."""
 
-    def __init__(self, config):
+    def __init__(self, config, event):
+        """Constructor.
+
+        Config is global config object.
+        """
         self.config = config
-
-    def mkdir(self, event):
+        self.event = event
         change_id = event["change"]["id"]
         self.cr_dir = os.path.join(self.config["dir"], change_id)
+        self.mkdir()
+
+    def mkdir(self):
         mkdir(self.cr_dir)
-        template = Template(filename=self.config["cr-template"])
-        header_filename = os.path.join(self.cr_dir, self.config["header-filename"])
-        with open(header_filename, "wb") as head:
-            head.write(template.render(event=event))
-        self.ps_dir = os.path.join(self.cr_dir, event["patchSet"]["number"])
+        header = os.path.join(self.cr_dir, self.config["header-filename"])
+        self.render(header, "cr-template", event=self.event)
+        self.ps_dir = os.path.join(self.cr_dir, self.event["patchSet"]["number"])
         mkdir(self.ps_dir)
-        template = Template(filename=self.config["ps-template"])
-        header_filename = os.path.join(self.ps_dir, self.config["header-filename"])
-        with open(header_filename, "wb") as head:
-            head.write(template.render(event=event))
+        header = os.path.join(self.ps_dir, self.config["header-filename"])
+        self.render(header, "ps-template", event=self.event)
 
     def stdout(self, job, name="log.txt.gz"):
         dirname = os.path.join(self.ps_dir, job["name"])
         filename = os.path.join(dirname, name)
         mkdir(dirname)
-        template = Template(filename=self.config["job-template"])
-        header_filename = os.path.join(dirname, self.config["header-filename"])
-        with open(header_filename, "wb") as head:
-            head.write(template.render(job=job))
+        header = os.path.join(dirname, self.config["header-filename"])
+        self.render(header, "job-template", job=job)
         return gzip.open(filename, "wb")
+
+    def publish_summary(self, jobs):
+        index = os.path.join(self.ps_dir, "index.html")
+        self.render(index, "summary-template", jobs=jobs, event=self.event)
+
+    def render(self, filename, template_name, **kwargs):
+        template = Template(filename=self.config[template_name])
+        with open(filename, "wb") as f:
+            f.write(template.render(**kwargs))
