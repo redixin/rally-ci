f627bcbf389eb8b04604223be2e012b15cfc2443
Sergey Skripnick <sskripnick@mirantis.com>
2015-09-21 17:08:37 +0300

    integration tests: status: add basic test

--- tests/integrated/test_services_status.py
+++ tests/integrated/test_services_status.py
@@ -11,53 +11,26 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import socket
-import unittest
+
+import urllib
+from urllib import request
+
+from tests.integrated import base
 
 
-def get_free_port():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("localhost", 0))
-    port = s.getsockname()[1]
-    s.close()
-    return port
+class StatusTestCase(base.IntegrationTest):
 
+    def _get_config(self):
+        port = base.get_free_port()
+        self.url = "http://localhost:%s" % port
+        conf = {
+                "service": {
+                    "name": "status",
+                    "module": "rallyci.services.status",
+                    "listen": ["localhost", port],
+                }
+        }
+        return [[conf], [port]]
 
-def wait_for_port(port, timeout=4):
-    deadline = time.time() + timeout
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while True:
-        if time.time() > deadline:
-            return
-        e = s.connect_ex(("localhost", port))
-        if e:
-            time.sleep(0.1)
-        else:
-            s.close()
-            return
-
-
-class IntegrationTest(unittest.TestCase):
-
-    def setUp(self):
-        self.tmpdir = tempfile.TemporaryDirectory(prefix="rci_tst_integration")
-        conf = self._get_config()
-        self.cf = os.path.join(self.tmpdir.name, "cf.yaml")
-        with open(self.cf, "w") as cfg:
-            cfg.write(yaml.dump(conf))
-        self.server_out = open(os.path.join(self.tmpdir.name, "out.log"), "w+")
-        self.server_err = open(os.path.join(self.tmpdir.name, "err.log"), "w+")
-        self.server = subprocess.Popen(["rally-ci", self.cf],
-                                       stdout=self.server_out,
-                                       stderr=self.server_err)
-        wait_for_port(port)
-
-    def tearDown(self):
-        self.server.terminate()
-        for fd in (self.server_err, self.server_out):
-            fd.seek(0)
-            print(fd.read())
-            fd.close()
-        with open(self.cf) as cf:
-            print(cf.read())
-        self.tmpdir.cleanup()
+    def test_index(self):
+        r = request.urlopen(self.url)
